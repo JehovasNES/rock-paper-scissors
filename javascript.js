ca13b13aa41af0f013c8830c26c001de4fca1ce7
JehovasNES <thomas.jfry.vanallen@gmail.com>
2022-02-06 23:22:09 -0600

    complete function for gameplay

--- javascript.js
+++ javascript.js
@@ -1,11 +1,33 @@
-function computerPlay(){
+
+
+function computerChoice(){
     let choice = ["rock", "paper", "scissors"]
     choiceMade = choice[Math.floor(Math.random() *  choice.length)];
     return choiceMade;
 }
 
-console.log(computerPlay());
 
-const rock = "rock";
-const paper = "paper";
-const scissors = "scissors";
+
+function playRound(playerSelect, computerSelect) {
+    playerSelect = playerSelect.toUpperCase();
+    computerSelect = computerSelect.toUpperCase();
+    if (playerSelect == computerSelect) {
+        return("Tie!");
+    }
+    else if (
+        (playerSelect == "ROCK" && computerSelect == "PAPER") ||
+        (playerSelect == "PAPER" && computerSelect == "SCISSORS") ||
+        (playerSelect == "SCISSORS" && computerSelect == "ROCK")
+    ) return("You Lose!");
+    else if (
+        (playerSelect == "ROCK" && computerSelect == "SCISSORS") ||
+        (playerSelect == "PAPER" && computerSelect == "ROCK") ||
+        (playerSelect == "SCISSORS" && computerSelect == "PAPER")
+    ) return("You Win!");
+    else return("Please enter a valid choice!");
+}
+
+const playerSelect = "Paper"
+const computerSelect = computerChoice();
+console.log(computerSelect);
+console.log(playRound(playerSelect, computerSelect));
