a932417d192afe1ae7f416d2a947d44d5cf2e600
JehovasNES <thomas.jfry.vanallen@gmail.com>
2022-02-08 20:47:21 -0600

    correct and add loop

--- javascript.js
+++ javascript.js
@@ -7,28 +7,31 @@ function computerChoice(){
 }
 
 
-
 function playRound(playerSelect, computerSelect) { //Does not seem to reroll computerChoice so it can be run again.
     playerSelect = playerSelect.toUpperCase(); //Dev Console will not run the program if you do not define playerSelect rerun. Seems like normal behavior
-    computerSelect = computerSelect.toUpperCase();
-    if (playerSelect == computerSelect) {
-        return("Tie!");
-    }
-    else if (
-        (playerSelect == "ROCK" && computerSelect == "PAPER") ||
-        (playerSelect == "PAPER" && computerSelect == "SCISSORS") ||
-        (playerSelect == "SCISSORS" && computerSelect == "ROCK")
-    ) return("You Lose! The score is now " + playerScore + "-" + ++computerScore); 
-    else if (
-        (playerSelect == "ROCK" && computerSelect == "SCISSORS") ||
-        (playerSelect == "PAPER" && computerSelect == "ROCK") ||
-        (playerSelect == "SCISSORS" && computerSelect == "PAPER")
-    ) return('You Win! The score is now ' + ++playerScore + '-' + computerScore);
-    else return("Please enter a valid choice!");
+    do {
+        computerSelect = computerChoice();
+        playerInput = window.prompt("Select Rock, Paper, or Scissors?");
+        if (playerSelect == computerSelect) {
+            return("Tie!");
+        }
+        else if (
+            (playerSelect == "ROCK" && computerSelect == "PAPER") ||
+            (playerSelect == "PAPER" && computerSelect == "SCISSORS") ||
+            (playerSelect == "SCISSORS" && computerSelect == "ROCK")
+        ) return("You Lose! The score is now " + playerScore + "-" + ++computerScore); 
+        else if (
+            (playerSelect == "ROCK" && computerSelect == "SCISSORS") ||
+            (playerSelect == "PAPER" && computerSelect == "ROCK") ||
+            (playerSelect == "SCISSORS" && computerSelect == "PAPER")
+        ) return('You Win! The score is now ' + ++playerScore + '-' + computerScore);
+        else return("Please enter a valid choice!");
+    } while (playerScore < 5 || computerScore < 5);
 }
 
 let playerScore = 0;
 let computerScore = 0;
+let sCount = scoreCount();
 
 function scoreCount() { //issue that needs to be fixed, doesn't seem to notice the pre-increment so it cannot be used as the primary score method yet
     if (playerScore >= 5)
