bdb9bd8b35d36c3d477b9fe39bc05ae033c106ca
JehovasNES <thomas.jfry.vanallen@gmail.com>
2022-02-11 04:11:46 -0600

    clean file and remove comments

--- javascript.js
+++ javascript.js
@@ -1,33 +1,35 @@
-function computerChoice(){
-    let choice = ["ROCK", "PAPER", "SCISSORS"] //array the three choices
-    choiceMade = choice[Math.floor(Math.random() *  choice.length)]; //generate a random number from 0-2 to select an option
-    return choiceMade;
+function computerInput(){
+    option = ["ROCK", "PAPER", "SCISSORS"]
+    selection = option[Math.floor(Math.random() *  option.length)];
+    return selection;
 }
 
-function playRound(playerSelect, computerSelect) {
-    playerSelect = playerSelect.toUpperCase();
+function playGame(playerSelect, compSelect) {
+    playerScore = 0;
+    compScore = 0;
     do {
-        computerSelect = computerChoice(); //assigns a new choice
-        playerInput = window.prompt("Select Rock, Paper, or Scissors?"); //prompts for input
+        compSelect = computerInput();
+        playerInput = window.prompt("Select Rock, Paper, or Scissors?");
         playerSelect = playerInput.toUpperCase();
-        
-        console.log(playerSelect); //test for playerSelect
-        console.log(computerSelect); //computerSelect
-        if (playerSelect == computerSelect) {
+
+        console.log(playerSelect); 
+        console.log(compSelect); 
+
+        if (playerSelect == compSelect) {
             alert("Tie!");
         }
         else if (
-            (playerSelect == "ROCK" && computerSelect == "PAPER") ||
-            (playerSelect == "PAPER" && computerSelect == "SCISSORS") ||
-            (playerSelect == "SCISSORS" && computerSelect == "ROCK")
-        ) alert("You Lose! The score is now " + playerScore + "-" + ++computerScore); 
+            (playerSelect == "ROCK" && compSelect == "PAPER") ||
+            (playerSelect == "PAPER" && compSelect == "SCISSORS") ||
+            (playerSelect == "SCISSORS" && compSelect == "ROCK")
+        ) alert("You Lose! The score is now " + playerScore + "-" + ++compScore); 
         else if (
-            (playerSelect == "ROCK" && computerSelect == "SCISSORS") ||
-            (playerSelect == "PAPER" && computerSelect == "ROCK") ||
-            (playerSelect == "SCISSORS" && computerSelect == "PAPER")
-        ) alert('You Win! The score is now ' + ++playerScore + '-' + computerScore);
+            (playerSelect == "ROCK" && compSelect == "SCISSORS") ||
+            (playerSelect == "PAPER" && compSelect == "ROCK") ||
+            (playerSelect == "SCISSORS" && compSelect == "PAPER")
+        ) alert('You Win! The score is now ' + ++playerScore + '-' + compScore);
         else alert("Please enter a valid choice!");
-    } while (playerScore < 5 && computerScore < 5);
+    } while (playerScore < 5 && compScore < 5);
         if (playerScore == 5)
             return alert("YOU WON!");
         else
@@ -35,10 +37,7 @@ function playRound(playerSelect, computerSelect) {
     
 }
 
-let playerScore = 0;
-let computerScore = 0;
-let playerInput = "";
-
-let playerSelect = playerInput; //convert to playerSelect for playRound function
-let computerSelect = computerChoice(); //convert to computerSelect for computerChoice function
-console.log(playRound(playerSelect, computerSelect));
+playerInput = "";
+playerSelect = playerInput;
+compSelect = computerInput();
+console.log(playGame(playerSelect, compSelect));
